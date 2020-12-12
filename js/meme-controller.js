f79aa8a7b19a9d2f2c76d7f9772de71c791b49a7
Eyal Alfasi <eyalalf@gmail.com>
2020-12-12 21:52:19 +0200

    Added filtering for images by keywords + responsive design improvements

--- js/meme-controller.js
+++ js/meme-controller.js
@@ -11,6 +11,7 @@ function init() {
     gCtx = gCanvas.getContext('2d');
     gCanvas.width = '400';
     gCanvas.height = '400';
+    renderImgsKeywords();
     renderImgsGallery();
 }
 
@@ -124,6 +125,21 @@ function renderImgsGallery() {
         return `<img src="${img.url}" onclick="onImgPick(${img.id})">`
     }).join('');
     elImgsContainer.innerHTML = strHTMLs;
+}
+
+function renderImgsKeywords() {
+    const elKeywordsContainer = document.querySelector('.keywords');
+    const keywords = getKeywords();
+    let strHTMLs = keywords.map(keyword => {
+        return `<span class="keyword" onclick="onFilterImgs(this)">${keyword}</span>\n`
+    }).join('');
+    elKeywordsContainer.innerHTML = strHTMLs;
+}
+
+function onFilterImgs(elKeyword){
+    const keyword = elKeyword.innerText;
+    setKeywordFilter(keyword);
+    renderImgsGallery();
 }
 
 function clearCanvas() {
--- js/meme-service.js
+++ js/meme-service.js
@@ -1,22 +1,23 @@
 'use strict'
 
 var gKeywords = { 'happy': 12, 'funny puk': 1 }
+var gCurrKeyWord;
 var gImgs = [
-    { id: 1, url: 'meme-imgs-square/1.jpg', keywords: ['happy'] },
+    { id: 1, url: 'meme-imgs-square/1.jpg', keywords: ['happy', 'sad', 'fun'] },
     { id: 2, url: 'meme-imgs-square/2.jpg', keywords: ['sad'] },
     { id: 3, url: 'meme-imgs-square/3.jpg', keywords: ['crazy'] },
     { id: 4, url: 'meme-imgs-square/4.jpg', keywords: ['fun'] },
-    { id: 5, url: 'meme-imgs-square/5.jpg', keywords: ['Music'] },
+    { id: 5, url: 'meme-imgs-square/5.jpg', keywords: ['music', 'crazy'] },
     { id: 6, url: 'meme-imgs-square/6.jpg', keywords: ['happy'] },
     { id: 7, url: 'meme-imgs-square/7.jpg', keywords: ['sad'] },
     { id: 8, url: 'meme-imgs-square/8.jpg', keywords: ['crazy'] },
     { id: 9, url: 'meme-imgs-square/9.jpg', keywords: ['fun'] },
-    { id: 10, url: 'meme-imgs-square/10.jpg', keywords: ['Music'] },
+    { id: 10, url: 'meme-imgs-square/10.jpg', keywords: ['music'] },
     { id: 11, url: 'meme-imgs-square/11.jpg', keywords: ['happy'] },
     { id: 12, url: 'meme-imgs-square/12.jpg', keywords: ['sad'] },
-    { id: 13, url: 'meme-imgs-square/13.jpg', keywords: ['crazy'] },
+    { id: 13, url: 'meme-imgs-square/13.jpg', keywords: ['crazy', 'music', 'fun'] },
     { id: 14, url: 'meme-imgs-square/14.jpg', keywords: ['fun'] },
-    { id: 15, url: 'meme-imgs-square/15.jpg', keywords: ['Music'] },
+    { id: 15, url: 'meme-imgs-square/15.jpg', keywords: ['music'] },
     { id: 16, url: 'meme-imgs-square/16.jpg', keywords: ['happy'] },
     { id: 17, url: 'meme-imgs-square/17.jpg', keywords: ['sad'] },
     { id: 18, url: 'meme-imgs-square/18.jpg', keywords: ['crazy'] },
@@ -29,7 +30,16 @@ function getCurrImgById() {
 }
 
 function getImgsGallery() {
-    return gImgs;
+    let filteredKeywords = gImgs.filter(img => {
+        return (img.keywords.findIndex(keyword => {
+            return gCurrKeyWord === keyword;
+        }) !== -1)
+    })
+    return (filteredKeywords.length === 0) ? gImgs : filteredKeywords;
+}
+
+function setKeywordFilter(keyword) {
+    gCurrKeyWord = keyword;
 }
 
 function setCurrMemeImg(imgId) {
@@ -172,4 +182,20 @@ function fontColorChange(color) {
 
 function strokeWidthChange(width) {
     gMeme.lines[gMeme.selectedLineIdx].strokeWidth = width;
+}
+
+function getKeywords() {
+    let keywords = [];
+    gImgs.forEach(img => {
+        keywords = [...keywords, ...img.keywords]
+    })
+
+    let newKeyWords = [];
+
+    keywords.forEach(keyword => {
+        (newKeyWords.findIndex(currKeyword => {
+            return currKeyword === keyword;
+        }) === -1) && newKeyWords.push(keyword);
+    })
+    return newKeyWords;
 }
